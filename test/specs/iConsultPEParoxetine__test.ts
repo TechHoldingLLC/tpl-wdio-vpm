d5836dc5fe45a3605fd47ad9bdfbfa6d5c51f6bc
priyankashah-th <priyanka.shah@techholding.co>
2024-06-18 18:09:13 +0530

    iConsult and Sign In - Tests Fix

--- test/specs/iConsultPEParoxetine__test.ts
+++ test/specs/iConsultPEParoxetine__test.ts
@@ -33,8 +33,10 @@ describe("iConsult Features", () => {
     await LoginPage.login(loginData.login_email, loginData.login_password);
 
     await browser.pause(3000);
+    await await iConsult.sidemenuCloseButton.waitForClickable({ timeout: 3000 });
     await iConsult.sidemenuCloseButton.click();
     await browser.pause(2000);
+    await iConsult.startFreeiConsultbutton.waitForClickable({ timeout: 3000 });
     await iConsult.startFreeiConsultbutton.click();
     await browser.pause(3000);
     await iConsult.consentCheckbox.click();
