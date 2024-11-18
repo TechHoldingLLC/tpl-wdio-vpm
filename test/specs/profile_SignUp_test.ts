6a3db6c099d78ef5de8378c8d07efc98528b9093
Priyanka Shah <Priyanka.shah@techholding.co>
2024-11-18 18:34:51 +0530

    signUp_code

---
+++ test/specs/profile_SignUp_test.ts
@@ -0,0 +1,27 @@
+// Test Case: Sign In: Verifying various login scenarios (C29652, C29650, C29651)
+// Description: This test suite covers different Sign In scenarios for the application including invalid credentials (email, mobile, password) and successful login with valid credentials.
+
+import { expect } from "@wdio/globals";
+import SignUpPage from "../pageobjects/signup.page.js";
+import LoginPage from "../pageobjects/login.page.js";
+
+import fs from "fs";
+
+describe("VPM Sign Up Features", () => {
+  let signupdata: any;
+
+  // Pre-condition: Load the login data from a JSON file
+  before(async () => {
+    signupdata = JSON.parse(
+      fs.readFileSync("./test/data/signUpData.json", "utf-8")
+    );
+  });
+
+  // Pre-condition for each test: Navigate to login page and open Sign In modal
+  beforeEach(async () => {
+    await browser.url(""); // Open the base URL
+    await browser.pause(4000);
+    await LoginPage.signinButton.click(); // Click the Sign In button
+    await browser.pause(2000); // Wait for login modal to appear
+  });
+});
